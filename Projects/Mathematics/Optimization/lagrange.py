25aad970c43ff787a9cdbc55f705fdf2346993a1
anton96vice <65628050+anton96vice@users.noreply.github.com>
2021-02-24 03:41:39 -0500

    Create lagrange.py

---
+++ Projects/Mathematics/Optimization/lagrange.py
@@ -0,0 +1,15 @@
+#minimization function utilizing Lagrange method
+import sympy as sp
+
+#min((x-2)^2+(y-1)^2) with x+y-1=0
+
+
+x,y,z = sp.symbols('x y z')
+L = (x-2)**2 + (y-1)**2 +z*(x+y-1)
+derivatives = list()
+for i in symbols:
+  derivatives.append(sp.diff(L,i))
+solution = sp.solve([x for x in derivatives], symbols)
+f = sp.lambdify([x,y,z],L)
+f(1,0,2)
+# Out: 2
