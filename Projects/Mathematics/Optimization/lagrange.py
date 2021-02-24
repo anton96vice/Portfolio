d05b8f91458623cc868d52e056cf9b5c249d3553
anton96vice <65628050+anton96vice@users.noreply.github.com>
2021-02-24 03:44:55 -0500

    Update lagrange.py

--- Projects/Mathematics/Optimization/lagrange.py
+++ Projects/Mathematics/Optimization/lagrange.py
@@ -1,4 +1,6 @@
 #minimization function utilizing Lagrange method
+# Problem:
+#НFind square distance from point (2,1) to straight line x+y=1  with lagrange
 import sympy as sp
 
 #min((x-2)^2+(y-1)^2) with x+y-1=0
